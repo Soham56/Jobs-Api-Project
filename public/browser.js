c7a1d09836d516b9e7aceb9412f482dbbec714ea
Soham <sohamdey567@gmail.com>
2023-10-27 20:40:03 +0530

    front-end responsiveness added

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,8 @@
+const preLoder = document.querySelector('.preloder');
+window.addEventListener('load', ()=>{
+    document.body.removeChild(preLoder);
+})
+
 var confettiSettings = { target: 'my-canvas' };
-    var confetti = new ConfettiGenerator(confettiSettings);
-    confetti.render();
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
